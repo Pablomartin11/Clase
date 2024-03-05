d0de83088b3b209b07b42e56716c0a38e2cea1cb
Pablomartin11 <pablo.martin.benito@estudiantes.uva.es>
2024-03-05 17:58:50 +0100

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,26 @@
+
+public class Cliente {
+
+public static final int PUERTO = 2001;
+
+
+public static void main(String[] args) {
+    String linea = null;
+    try {
+        java.io.BufferedReader tec =
+        new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+        java.net.Socket miSocket = new java.net.Socket("157.88.125.192", PUERTO);
+
+        java.io.BufferedReader inred = new java.io.BufferedReader(new java.io.InputStreamReader(miSocket.getInputStream()));
+            java.io.PrintStream outred = new java.io.PrintStream(miSocket.getOutputStream());
+            while ((linea = tec.readLine()) != null) { // lee de teclado
+                outred.println(linea);// envia al servidor
+                linea = inred.readLine();// lee del servidor
+                System.out.println("Recibido: "+linea);// eco local del servidor
+            }
+
+        } catch (Exception e) { e.printStackTrace(); }
+
+
+    }
+}
